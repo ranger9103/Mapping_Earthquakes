b079c0a7861a140f5b440a582f1cb221328e8a97
Ranger9103 <kateheise@yahoo.com>
2022-06-11 23:29:47 -0500

    13.4.3

--- Mapping_lines/static/js/logic.js
+++ Mapping_lines/static/js/logic.js
@@ -1,8 +1,31 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level.
-let map = L.map("mapid").setView([40.7, -94.5], 4);
+// Create the map object with options
+let map = L.map('mapid').setView([39.0997, -94.5786], 4.5);
+
+// Coordinates for each point to be used in the line.
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+];
+
+L.polyline(line, {
+    color: 'yellow'
+}).addTo(map);
+
+let line2 = [
+    [40.6413, -73.7781],
+    [43.6777, -79.6248],
+    [44.2941, -92.6691],
+    [25.2744, -133.7751]
+];
+
+L.polyline(line2, {
+    color: 'blue'
+}).addTo(map);
 
 // An array containing each city's location, state, and population.
 //see cities.js
@@ -27,8 +50,8 @@ cityData.forEach(function(city) {
 //  Add a marker to the map for Zumbrota MN.
 let ZMmarker = L.marker([44.2941, -92.6691]).addTo(map);
 
-// We create the tile layer that will be the background of our map. night mode
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
